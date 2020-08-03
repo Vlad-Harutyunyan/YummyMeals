955f0cce1b765c701d9e3b685d9043d11c5f34dc
JoraKaryan <63347929+JoraKaryan@users.noreply.github.com>
2020-08-03 18:19:00 +0400

    Add files via upload

--- app/mail/routes.py
+++ app/mail/routes.py
@@ -1,3 +1,8 @@
+import os
+
+import schedule
+import time
+
 from flask import (
     Flask,
     g,
@@ -6,32 +11,27 @@ from flask import (
     render_template,
     Blueprint, current_app
 )
-import schedule
-import os
 from flask_login import current_user
 from flask_mail import Mail, Message
-from ..users.models import User,User_Favorite_Category
-from ..meals.models import Meal,Category
+from ..users.models import User, User_Favorite_Category
+from ..meals.models import Meal, Category
 import datetime
 from datetime import timedelta
-import time
 
-satatic_path = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static'))
+
+satatic_path = os.path.abspath(os.path.join(os.path.dirname(
+    os.path.abspath(__file__)), 'static'))
+
 
 class threadClass:
-
     def __init__(self):
         thread = threading.Thread(target=self.run, args=())
-        thread.daemon = True                       # Daemonize thread
-        thread.start()                             # Start the execution
+        thread.daemon = True
+        thread.start()
 
+# This might take several minutes to complete
     def run(self):
-
-         #
-         # This might take several minutes to complete
-         someHeavyFunction()
-
-
+        someHeavyFunction()
 
 mail_bp = Blueprint(
     'mail',
@@ -44,25 +44,30 @@ mail_bp = Blueprint(
 
 
 def mail_send():
-    app=current_app._get_current_object()
-    mail=Mail(app)
-    fav=User_Favorite_Category.query.all()
-    time_trigger=datetime.datetime.utcnow()- timedelta(hours=24)
-    updated_list=[]
-    updated=Meal.query.filter(Meal.date_posted>time_trigger).all()
+    app = current_app._get_current_object()
+    mail = Mail(app)
+    fav = User_Favorite_Category.query.all()
+    time_trigger = datetime.datetime.utcnow() - timedelta(hours=24)
+    updated_list = []
+    updated = Meal.query.filter(Meal.date_posted > time_trigger).all()
     for j in updated:
         updated_list.append(j.category_id)
-    updated_list=list(set(updated_list))
+    updated_list = list(set(updated_list))
     for x in fav:
         if x.category_id in updated_list:
             print("ok")
-            msg = Message('New Recipe', sender='yummymealbook@gmail.com',
-                          recipients=[f'{User.query.filter(User.id==x.user_id).first().email}'])
-            msg.body = f'Hello dear {User.query.filter(User.id==x.user_id).first().username}!  We have good news for you!   A new recipe for your selected category "{Category.query.filter(Category.id==x.category_id).first().name}" has arrived!'
-            mail.send(msg)
+            msg = Message(
+                'New Recipe', sender='yummymealbook@gmail.com', recipients=[
+                    f'{User.query.filter(User.id ==x.user_id).first().email}'])
+            msg.body = f'Hello dear' \
+           f' {User.query.filter(User.id == x.user_id).first().username}!' \
+           f' We have good news for you! A new recipe for your' \
+           f' selected category' \
+           f'{Category.query.filter(Category.id==x.category_id).first().name}'\
+           f' has arrived!'
+        mail.send(msg)
 
     return f'mails sent {datetime.datetime.utcnow()}'
-
 
 # schedule.every(60).minutes.do(index)
 # while True:
