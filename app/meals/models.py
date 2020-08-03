8871e7490a389fbbc7283ab272b6eeb1bec71abd
RuzannaO <63346028+RuzannaO@users.noreply.github.com>
2020-08-03 20:15:12 +0400

    Update models.py

--- app/meals/models.py
+++ app/meals/models.py
@@ -1,29 +1,28 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, ForeignKey ,create_engine
+from sqlalchemy import Column, Integer, String, ForeignKey, create_engine
 from sqlalchemy.orm import sessionmaker, relationship
+from datetime import datetime
 import os
 import json
 from .. import db
 from ..users.models import User
-from datetime import datetime
 
 
 class Area(db.Model):
-
     __tablename__ = 'area'
-
+    __tablename__ = 'area'
     id = Column(Integer, primary_key=True)
     name = Column(String)
 
     def __repr__(self):
             return f"Area '{self.name}','{self.id}' "
 
+
 class Category(db.Model):
     __tablename__ = 'category'
-
     id = Column(Integer, primary_key=True)
     name = Column(String)
-    img_link =  Column(String)
+    img_link = Column(String)
     description = Column(String)
 
     def __repr__(self):
@@ -32,26 +31,26 @@ class Category(db.Model):
 
 class Ingredient(db.Model):
     __tablename__ = 'ingredient'
-
     id = Column(Integer, primary_key=True)
     name = Column(String)
     description = Column(String)
- 
+
     def __repr__(self):
             return f"Ingredient '{self.name} , {self.description}'  "
+
 
 class Meal_ingredient(db.Model):
     __tablename__ = 'meal_ingredient'
 
-    id = Column(Integer, primary_key = True)
-    by_user = Column(Integer, default = 0 )
-    meal_id = Column(String,ForeignKey('meal.id'))
-    ingredient_id = Column(String,ForeignKey('ingredient.id'))
+    id = Column(Integer, primary_key=True)
+    by_user = Column(Integer, default=0)
+    meal_id = Column(String, ForeignKey('meal.id'))
+    ingredient_id = Column(String, ForeignKey('ingredient.id'))
     meal = relationship("Meal")
     ingredient = relationship("Ingredient")
 
     def __repr__(self):
-            return f"Meal ingr '{self.meal.name}','{self.ingredient.name}' "
+            return f"Meal ingr '{self.meal.name}', '{self.ingredient.name}' "
 
 
 class Meal(db.Model):
@@ -59,11 +58,11 @@ class Meal(db.Model):
 
     id = Column(Integer, primary_key=True)
     name = Column(String)
-    category_id =  Column(Integer, ForeignKey('category.id'))
+    category_id = Column(Integer, ForeignKey('category.id'))
     area_id = Column(Integer, ForeignKey('area.id'))
-    author_id = Column(Integer, ForeignKey('user.id'), default = 1)
+    author_id = Column(Integer, ForeignKey('user.id'), default=1)
     instructions = Column(String)
-    img_link = Column(String , default = 'default.jpg')
+    img_link = Column(String, default='default.jpg')
     tags = Column(String)
     video_link = Column(String)
     date_posted = Column(db.DateTime, nullable=False, default=datetime.utcnow)
@@ -72,14 +71,8 @@ class Meal(db.Model):
     user = relationship("User")
 
     def __repr__(self):
-            return f"Meal '{self.id}','{self.name}','{self.user.username}','{self.date_posted}'  "
+            return f"Meal '{self.id}', '{self.name}', '{self.user.username}', /
+            /'{self.date_posted}'"
 
-
-
-
-
-
-path = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)),"..",".."))
-
-
-
+path = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                                    "..", ".."))
