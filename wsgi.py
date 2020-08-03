b493675f9a157b11dd22609edfc65d619ebce717
JoraKaryan <63347929+JoraKaryan@users.noreply.github.com>
2020-08-03 18:20:52 +0400

    Add files via upload

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,7 @@
 from app import create_app
 
 app = create_app()
-
-
-#commands  flask db init , flask db migrate , flask db upgrade
+# commands  flask db init, flask db migrate, flask db upgrade
 
 if __name__ == "__main__":
-   app.run(port=5000,debug=True)
+   app.run(port=5000, debug=True)
