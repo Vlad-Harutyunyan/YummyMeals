ff39d36f97979eb04aa35dcac78d13c2175ebfff
Vlad <43403715+Vlad-Harutyunyan@users.noreply.github.com>
2020-08-05 11:20:54 +0400

    Vlad:Update [PEP8 Style] meals-routes

--- app/meals/routes.py
+++ app/meals/routes.py
@@ -20,10 +20,10 @@ from .lib.funct import name_correct
 from ..users.forms import CommentForm
 from ..users.models import UserComments
 
-stat_path = os.path.abspath(os.path.join(
-os.path.dirname(
-os.path.abspath(__file__)),
-'static'))
+satatic_path = os.path.abspath(
+                                os.path.join(
+                                    os.path.dirname(
+                                        os.path.abspath(__file__)), 'static'))
 
 meals_bp = Blueprint(
     'meals',
@@ -33,6 +33,7 @@ meals_bp = Blueprint(
     static_folder='static',
     static_url_path=stat_path
 )
+
 
 def get_fav_catgories(category_id):
     check = db.session.query(User_Favorite_Category).filter(
@@ -63,9 +64,14 @@ def test_route():
 @meals_bp.route('/categories')
 def categories_list():
     categories = Category.query.all()
-    checker = db.session.query(Category).join(User_Favorite_Category).filter_by(user_id = current_user.id).all() 
-    return render_template('category_list.html', categories=categories , checker = checker)
-  
+    checker = db.session.query(Category).\
+        join(User_Favorite_Category).\
+        filter_by(user_id=current_user.id).all()
+    return render_template(
+        'category_list.html',
+        categories=categories,
+        checker=checker)
+
 
 @meals_bp.route('/search_by_fullname/<meal_name>/')
 def meal_search_by_fullname(meal_name):
@@ -75,19 +81,30 @@ def meal_search_by_fullname(meal_name):
         form = CommentForm()
         check = False
         page = request.args.get('page', 1, type=int)
-        comments = UserComments.query.filter(UserComments.meal_id == meal.id).paginate(per_page=2, page=page)
-        ingredients = Meal_ingredient.query.filter_by(meal_id=meal.id).all()
-        return render_template('meal_info.html', meal=meal, ingredients=ingredients,
-                           form=form,check = check,comments=comments,page=page)
+        comments = UserComments.query.filter(
+            UserComments.meal_id == meal.id)\
+            .paginate(per_page=2, page=page)
+        ingredients = Meal_ingredient.query.filter_by(
+            meal_id=meal.id).all()
+        return render_template(
+            'meal_info.html',
+            meal=meal,
+            ingredients=ingredients,
+            form=form,
+            check=check,
+            comments=comments,
+            page=page)
     else:
         return redirect('/meal')
 
 
 @meals_bp.route('/search/')
 def meal_search():
-    meal_name =  request.args.get('meal_name')
+    meal_name = request.args.get('meal_name')
     page = request.args.get('page', 1, type=int)
-    meallist = db.session.query(Meal).filter(Meal.name.contains(meal_name.lower())).paginate(page, 6, False)
+    meallist = db.session.query(Meal).filter(
+        Meal.name.contains(meal_name.lower()))\
+        .paginate(page, 6, False)
     if meallist:
         next_url = url_for('meals.test_route', page=meallist.next_num) \
             if meallist.has_next else None
@@ -95,51 +112,58 @@ def meal_search():
             if meallist.has_prev else None
 
         return render_template(
-        'main_page.html',
-        meallist=meallist,
-        next_url=next_url,
-        prev_url=prev_url,
-    )
+            'main_page.html',
+            meallist=meallist,
+            next_url=next_url,
+            prev_url=prev_url,)
     else:
         return redirect('/meal')
 
+
 @meals_bp.route('/search_by_username/')
 def meal_search_by_username():
-    u_name =  request.args.get('srch_user_username')
+    u_name = request.args.get('srch_user_username')
     page = request.args.get('page', 1, type=int)
-    meallist = None 
-    if isinstance(u_name,str) and not u_name.isdecimal():
-        meallist = db.session.query(Meal).join(User).filter(User.username.contains(u_name.lower())).paginate(page, 6, False)
+    meallist = None
+    if isinstance(u_name, str) and not u_name.isdecimal():
+        meallist = db.session.query(Meal)\
+            .join(User).filter(User.username.contains(u_name.lower()))\
+            .paginate(page, 6, False)
     elif u_name.isdecimal():
-        meallist = db.session.query(Meal).join(User).filter(User.id.like(int(u_name))).paginate(page, 6, False)
-    if meallist:    
+        meallist = db.session.query(Meal)\
+            .join(User).filter(User.id.like(int(u_name)))\
+            .paginate(page, 6, False)
+    if meallist:
         next_url = url_for('meals.test_route', page=meallist.next_num) \
             if meallist.has_next else None
         prev_url = url_for('meals.test_route', page=meallist.prev_num) \
             if meallist.has_prev else None
-        
         return render_template(
-        'main_page.html',
-        meallist=meallist,
-        next_url=next_url,
-        prev_url=prev_url,
-    )
+            'main_page.html',
+            meallist=meallist,
+            next_url=next_url,
+            prev_url=prev_url)
     else:
         return redirect('/meal')
+
 
 @meals_bp.route('/category/<int:c_id>/')
 def meals_by_category(c_id):
     page = request.args.get('page', 1, type=int)
 
     meallist = Meal.query.filter_by(category_id=c_id).paginate(page, 6, False)
-    next_url = url_for('meals.meals_by_category', c_id=c_id, page=meallist.next_num) \
+    next_url = url_for(
+        'meals.meals_by_category',
+        c_id=c_id,
+        page=meallist.next_num)\
         if meallist.has_next else None
-    prev_url = url_for('meals.meals_by_category', c_id=c_id, page=meallist.prev_num) \
+    prev_url = url_for(
+        'meals.meals_by_category',
+        c_id=c_id,
+        page=meallist.prev_num)\
         if meallist.has_prev else None
 
-
     return render_template(
-
         'main_page.html',
         meallist=meallist,
         next_url=next_url,
@@ -172,9 +196,9 @@ def add_favorite(meal_id):
         db.session.commit()
     print(request.referrer)
 
-    if 'meal/meal_info' in request.referrer :
+    if 'meal/meal_info' in request.referrer:
         return redirect(url_for('meals.meal_info', m_id=meal_id))
-    else :
+    else:
         return redirect(url_for('users.users_profiles', u_id=current_user.id))
 
 
@@ -183,22 +207,24 @@ def meal_info(m_id):
     meal = Meal.query.filter_by(id=m_id).first()
     ingredients = Meal_ingredient.query.filter_by(meal_id=m_id).all()
     form = CommentForm()
-    try :
+    try:
         check = db.session.query(User_Favorite).filter(
             User_Favorite.meal_id.like(m_id),
             User_Favorite.user_id.like(current_user.id)
         ).first()
-    except :
+    except:
         check = False
     page = request.args.get('page', 1, type=int)
     comments = UserComments.query.\
         filter(UserComments.meal_id == m_id).\
         order_by(UserComments.date_posted.desc()).\
         paginate(per_page=2, page=page)
-    fav_count = len(db.session.query(User_Favorite).filter(User_Favorite.meal_id.like(m_id)).all())
+    fav_count = len(db.session.query(User_Favorite).filter(
+        User_Favorite.meal_id.like(m_id)).all())
     return render_template('meal_info.html',
                            meal=meal, ingredients=ingredients, check=check,
-                           form=form, comments=comments, page=page, m_id=m_id ,fav_count = fav_count)
+                           form=form, comments=comments, page=page,
+                           m_id=m_id, fav_count=fav_count)
 
 
 @meals_bp.route('/meal_info/<int:m_id>/', methods=['POST'])
@@ -206,7 +232,10 @@ def meal_info(m_id):
 def meal_info_post(m_id):
     form = CommentForm()
     if form.is_submitted():
-        comment = UserComments(content=form.content.data, user_id=current_user.id, meal_id=m_id)
+        comment = UserComments(
+            content=form.content.data,
+            user_id=current_user.id,
+            meal_id=m_id)
         db.session.add(comment)
         db.session.commit()
         return redirect(url_for('meals.meal_info',
@@ -236,16 +265,15 @@ def add_favorite_category(category_id):
         db.session.delete(bb)
         db.session.commit()
 
-    if 'meal/categories' in request.referrer :
+    if 'meal/categories' in request.referrer:
         return redirect(url_for('meals.categories_list'))
-    else : 
+    else:
         return redirect(url_for('users.users_profiles', u_id=current_user.id))
-   
 
 
 @meals_bp.route('/search/<meal_name>/')
 def meal_search_name(meal_name):
-    if not meal_name in name_correct(meal_name):
+    if meal_name not in name_correct(meal_name):
         return redirect('/meal')
     else:
         for name1 in name_correct(meal_name):
@@ -254,15 +282,19 @@ def meal_search_name(meal_name):
                 form = CommentForm()
                 check = False
                 page = request.args.get('page', 1, type=int)
-                comments = UserComments.query.filter(UserComments.meal_id == meal.id).paginate(per_page=2, page=page)
-                ingredients = Meal_ingredient.query.filter_by(meal_id=meal.id).all()
-                return redirect(url_for('meals.meal_info',m_id=meal.id))
+                comments = UserComments.query.filter(
+                    UserComments.meal_id == meal.id).\
+                    paginate(per_page=2, page=page)
+                ingredients = Meal_ingredient.query.filter_by(
+                    meal_id=meal.id).all()
+                return redirect(url_for('meals.meal_info',  m_id=meal.id))
 
 
 @meals_bp.route('/ingredient/<int:ing_id>/')
 @login_required
 def ingr_info(ing_id):
     ingredient = Ingredient.query.filter_by(id=int(ing_id)).first()
-    return render_template('ingredient_info.html', name=ingredient.name, description=ingredient.description )
-            
-            
+    return render_template(
+        'ingredient_info.html',
+        name=ingredient.name,
+        description=ingredient.description)
