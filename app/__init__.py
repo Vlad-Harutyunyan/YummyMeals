8355171a795d3604077f9a4f8553eda96a97d536
Vlad <43403715+Vlad-Harutyunyan@users.noreply.github.com>
2020-08-06 15:09:27 +0400

    Vlad:add [Part4] online chat - init.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
-import os
+from gevent import monkey
+monkey.patch_all()
 
+import os
 from datetime import datetime
 
 from flask import Flask, Blueprint, redirect, render_template
@@ -10,6 +12,8 @@ from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from flask_mail import Mail
 from flask_admin import Admin
+from flask_socketio import SocketIO
+
 from app.config import Config
 
 
@@ -19,27 +23,29 @@ bcrypt = Bcrypt()
 migrate = Migrate()
 mail = Mail()
 admin = Admin(name='Admin_panel', template_mode='bootstrap3')
+socketio = SocketIO(async_mode='gevent',always_connect=True)
 
 
-def create_app():
-    """Construct the core app object."""
+def create_app(debug= False,port= 5010):
+    """Construct the  core app object."""
 
     app = Flask(__name__)
     app.config.from_object(Config)
-
+    app.debug = debug
+    app.port= port
     login_manager.login_view = 'users.login'
     login_manager.login_message_category = 'info'
-
-
-# Initialize Plugins
+    
+     # Initialize Plugins
     db.init_app(app)
     login_manager.init_app(app)
     bcrypt.init_app(app)
     mail.init_app(app)
-
+    
     manager = Manager(app)
     manager.add_command('db', MigrateCommand)
     migrate.init_app(app, db)
+    socketio.init_app(app)
 
 
 # handle login_requerd for blueprint
@@ -97,11 +103,14 @@ def create_app():
 # admin panel
 
     with app.app_context():
+        
+
         from .users.routes import users_bp
         from .index.routes import index_bp
         from .meals.routes import meals_bp
         from .errors.routes import errors_bp
         from .mail.routes import mail_bp
+
 
 # admin panel register
         from .admin_panel.routes import AdminIndexPage, adm
@@ -118,6 +127,8 @@ def create_app():
         app.register_blueprint(meals_bp)
         app.register_blueprint(mail_bp)
         app.register_blueprint(adm)
+
+        
 
         db.create_all()
         return app
