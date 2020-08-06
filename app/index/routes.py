7c7f634cc8c46515ad491558798d4a269b2f3fed
Vlad <43403715+Vlad-Harutyunyan@users.noreply.github.com>
2020-08-06 15:10:51 +0400

    Vlad:add [Part6] online chat - index folder

--- app/index/routes.py
+++ app/index/routes.py
@@ -1,19 +1,25 @@
 import os
 import random
 from operator import itemgetter
+
 from flask import (
     Flask,
     g,
     url_for,
     redirect,
     render_template,
-    Blueprint
+    Blueprint,
+    session
 )
-from flask_login import current_user
+from flask_login import current_user, login_required
+from flask_socketio import SocketIO
 from sqlalchemy import func, desc, and_, select
+
 from .. import db
 from ..meals.models import Meal
 from ..users.models import UserComments, User_Favorite, User
+from ..chat import events
+
 
 static_path = os.path.abspath(os.path.join(os.path.dirname(
     os.path.abspath(__file__)), 'static'))
@@ -34,7 +40,25 @@ def index():
         mydict[record.meal_id] = len(User_Favorite.query.
                                      filter_by(meal_id=record.meal_id).all())
     res = dict(sorted(mydict.items(), key=itemgetter(1), reverse=True)[:5])
-    tops = Meal.query.filter(Meal.id.in_(list(res.keys()))).all()
+    tops = Meal.query.filter(Meal.id.in_(list(res.keys()))).all() 
     tops = sorted(tops, key=lambda o: list(res.keys()).index(o.id))
-    return render_template('index_page.html', tops=tops)
 
+    #chat
+    name = 'Guest'
+    if current_user.is_authenticated:
+        name =  current_user.username
+    room =  'main_room'  
+
+    session['name'] = name
+    session['room'] = room
+    online_count = len(events.clients)
+    print(online_count)
+    name = session.get('name', '')
+    room = session.get('room', '')
+    
+    return render_template(
+        'index_page.html',
+        tops=tops,
+        name=name,
+        room=room,
+        online_count=online_count)
