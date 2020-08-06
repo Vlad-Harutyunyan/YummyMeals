fb55bb69e578574a0ae896357294a02c804332dc
Vlad <43403715+Vlad-Harutyunyan@users.noreply.github.com>
2020-08-06 15:06:27 +0400

    Vlad:add [Part1] online chat - wsgi update

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,7 @@
-from app import create_app
+from app import create_app , socketio
 
-app = create_app()
-# commands  flask db init, flask db migrate, flask db upgrade
+app = create_app(port=5008,debug=True)
 
 if __name__ == "__main__":
-   app.run(port=5000, debug=True)
+   socketio.run(app)
+
