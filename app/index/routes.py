5336078a6911b75f5650f61740af2c2b9f9f1f3e
RuzannaO <63346028+RuzannaO@users.noreply.github.com>
2020-08-20 18:19:47 +0400

    Add files via upload

--- app/index/routes.py
+++ app/index/routes.py
@@ -1,14 +1,26 @@
 import os
+import random
 from operator import itemgetter
+import json
+
 from flask import (
+    Flask,
+    g,
+    url_for,
     redirect,
     render_template,
     Blueprint,
     session
 )
-from flask_login import current_user
+from flask_login import current_user, login_required
+from flask_socketio import SocketIO
+from sqlalchemy import func, desc, and_, select
+
+from .. import db
 from ..meals.models import Meal
-from ..users.models import UserFavorite
+from ..users.models import UserComments, User_Favorite, User
+from ..chat import events
+
 
 static_path = os.path.abspath(os.path.join(os.path.dirname(
     os.path.abspath(__file__)), 'static'))
@@ -23,17 +35,23 @@ index_bp = Blueprint(
 )
 
 
-@index_bp.route('/')
-def index():
+def top_fives():
     mydict = {}
-    for record in UserFavorite.query.all():
-        mydict[record.meal_id] = len(UserFavorite.query.
+    for record in User_Favorite.query.all():
+        mydict[record.meal_id] = len(User_Favorite.query.
                                      filter_by(meal_id=record.meal_id).all())
     res = dict(sorted(mydict.items(), key=itemgetter(1), reverse=True)[:5])
     tops = Meal.query.filter(Meal.id.in_(list(res.keys()))).all()
     tops = sorted(tops, key=lambda o: list(res.keys()).index(o.id))
+    print(tops)
+    return tops
 
-    # chat
+
+@index_bp.route('/')
+def index():
+    tops = top_fives()
+
+    #chat
     name = 'Guest'
     if current_user.is_authenticated:
         name = current_user.username
@@ -53,5 +71,4 @@ def index():
 
 @index_bp.route('/presentation')
 def presentation():
-    return redirect(
-        'https://view.genial.ly/5f2f99770c24590d87b624b9/presentation-yummy-meals')
+    return redirect('https://view.genial.ly/5f2f99770c24590d87b624b9/presentation-yummy-meals')
