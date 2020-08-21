2af0b963e59acdde04196a1be109a05bb474acf8
RuzannaO <63346028+RuzannaO@users.noreply.github.com>
2020-08-21 12:10:05 +0400

    Update models.py

--- app/users/models.py
+++ app/users/models.py
@@ -17,7 +17,7 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     image_file = db.Column(
-        db.String(20), nullable=False, default='default.jpg')
+        db.String(20), nullable=False, default='default.png')
     password = db.Column(db.String(60), nullable=False)
     comment = db.relationship('UserComments', backref='author', lazy=True)
     is_admin = db.Column(db.Boolean, default=False)
